9b73e67618ecb5c600ea552b2cc1340e764e0f4c
Ruben Torres Frias <isctorres@gmail.com>
2023-11-13 19:01:04 -0600

    abrir pagina web

--- lib/screens/intenciones_screen.dart
+++ lib/screens/intenciones_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class IntencionesScreen extends StatelessWidget {
   const IntencionesScreen({super.key});
@@ -15,39 +16,74 @@ class IntencionesScreen extends StatelessWidget {
           cardIntencion(
             color: Colors.amber,
             subtitle: "Abrir página web", 
-            title: "https://itcelaya.edu.mx"),
+            title: "https://itcelaya.edu.mx",
+            img: Image.asset('assets/291715_chrome_google_browser_logo_network_icon.png'),
+            evento: (){
+              _openWEB();
+              print('entrp');
+            }),
           const SizedBox(height: 5,),
           cardIntencion(
             title: "Llamada telefonica",
             subtitle: "4612279093",
-            color: Colors.orange
+            color: Colors.orange,
+            img: Image.asset('assets/67154_yellow_phone_icon.png'),
+            evento: (){}
             ),
           const SizedBox(height: 5,),
           cardIntencion(
             color: Colors.red,
             title: "Enviar SMS",
-            subtitle: "4612279093"
+            subtitle: "4612279093",
+            img: Image.asset('assets/1215419_chat_facetime_messages_sms_icon.png'),
+            evento: (){}
+          ),
+          const SizedBox(height: 5,),
+          cardIntencion(
+            color: Colors.greenAccent,
+            title: "Enviar Email",
+            subtitle: "4612279093",
+            img: Image.asset('assets/1215419_chat_facetime_messages_sms_icon.png'),
+            evento: (){}
+          ),
+          const SizedBox(height: 5,),
+          cardIntencion(
+            color: Colors.blueGrey,
+            title: "Tomar Fotografia",
+            subtitle: "4612279093",
+            img: Image.asset('assets/1215419_chat_facetime_messages_sms_icon.png'),
+            evento: (){}
           ),
         ],
       ),
     );
   }
 
+  Future<void> _openWEB() async {
+    Uri url = Uri.parse("https://itcelaya.edu.mx");
+    if( !await launchUrl(url)  ){
+      print("Exito");
+    }
+  }
+
   Widget cardIntencion({
     required Color color, 
     required String title, 
-    required String subtitle}){
+    required String subtitle,
+    required Widget img,
+    required Function() evento}){
     return Container(
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text(subtitle),
-        trailing: Icon(Icons.chevron_right),
-        leading: Image.asset('assets/291715_chrome_google_browser_logo_network_icon.png')
-      ),
       height: 80,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: color
+      ),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        trailing: Icon(Icons.chevron_right),
+        leading: img,
+        onTap: evento,
       ),
     );  
   }
