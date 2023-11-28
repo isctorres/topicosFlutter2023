12562544ef597f21d2a784da25b391d5cfdb5103
Ruben Torres Frias <isctorres@gmail.com>
2023-11-28 17:50:10 -0600

    api

--- lib/network/popular_api.dart
+++ lib/network/popular_api.dart
@@ -1,16 +1,19 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:topicos/modelos/popular_model.dart';
 
 class PopularApi {
 
-  final url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=&language=es-MX&page=1');
+  final url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=5019e68de7bc112f4e4337a500b96c56&language=es-MX&page=1');
 
-  Future<??> getAllPopular() async {
+  Future<List<PopularModel>?> getAllPopular() async {
     var res = await http.get(url);
     if( res.statusCode == 200 ){
       var jsonResult = jsonDecode(res.body)['results'] as List;
-      jsonResult.map((movie) => null)
+      return jsonResult.map((movie) => PopularModel.fromMap(movie)).toList();
+    }else{
+      return null;
     }
   }
 }
--- lib/screens/popular_screen.dart
+++ lib/screens/popular_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:topicos/modelos/popular_model.dart';
+import 'package:topicos/network/popular_api.dart';
 
 class PopularScreen extends StatefulWidget {
   const PopularScreen({super.key});
@@ -8,10 +10,41 @@ class PopularScreen extends StatefulWidget {
 }
 
 class _PopularScreenState extends State<PopularScreen> {
+
+  PopularApi? popularApi;
+
+  @override
+  void initState() {
+    super.initState();
+    popularApi = PopularApi();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Popular Movies'),),
+      body: FutureBuilder(
+        future: popularApi!.getAllPopular(),
+        builder:(context, AsyncSnapshot<List<PopularModel>?> snapshot) {
+          if( snapshot.hasData ){
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Text(snapshot.data![index].title!);
+              },
+            );
+          }else{
+            if( snapshot.hasError ){
+              return const Center(child: Text('Algo salio mal'),);
+            }
+            else{
+              return const Center(
+                child: CircularProgressIndicator()
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
